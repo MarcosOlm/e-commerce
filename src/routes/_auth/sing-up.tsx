9330d5f65307e87dc95ba2113b6ebc455800c9a9
unknown <marcoslimado12384@gmail.com>
2026-01-12 14:20:54 -0300

    feat(page): formulário para cadastrar uma conta

--- src/routes/_auth/sing-up.tsx
+++ src/routes/_auth/sing-up.tsx
@@ -1,9 +1,45 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button';
+import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth/sing-up')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/sing-up"!</div>
+  
+
+  return (
+    <>
+      <div className="w-full">
+        <form>
+          <FieldSet>
+            <FieldLegend className="text-center">Crie sua conta</FieldLegend>
+            <FieldDescription className="text-center">Cadastre-se para começar a comprar</FieldDescription>
+            <FieldGroup>
+              <Field>
+                <FieldLabel>Nome:</FieldLabel>
+                <Input placeholder="seu nome"/>
+              </Field>
+              <Field>
+                <FieldLabel>Email:</FieldLabel>
+                <Input placeholder="seu@email.com"/>
+              </Field>
+              <Field>
+                <FieldLabel>Senha:</FieldLabel>
+                <Input placeholder="*******"/>
+              </Field>
+              <Field>
+                <FieldLabel>Confirmar Senha:</FieldLabel>
+                <Input placeholder="*******"/>
+              </Field>
+              <Button>Entrar</Button>
+              <Link to="/sing-in" className="text-center hover:underline">Já tem conta?</Link>
+            </FieldGroup>
+          </FieldSet>
+        </form>
+      </div>
+    </>
+  );
 }
