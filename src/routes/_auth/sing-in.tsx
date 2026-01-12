800ebf6387fc6f08cafb8c519a52cbf146ba90b4
unknown <marcoslimado12384@gmail.com>
2026-01-12 14:20:36 -0300

    feat(page): formulário para logar na conta

--- src/routes/_auth/sing-in.tsx
+++ src/routes/_auth/sing-in.tsx
@@ -1,9 +1,36 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_auth/sing-in')({
+
+export const Route = createFileRoute("/_auth/sing-in")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_auth/sing-in"!</div>
+  return (
+    <>
+      <div className="w-full">
+        <form>
+          <FieldSet>
+            <FieldLegend className="text-center">Bem-vindo de volta!</FieldLegend>
+            <FieldDescription className="text-center">Entre para acessar sua conta</FieldDescription>
+            <FieldGroup>
+              <Field>
+                <FieldLabel>Email:</FieldLabel>
+                <Input placeholder="seu@email.com"/>
+              </Field>
+              <Field>
+                <FieldLabel>Senha:</FieldLabel>
+                <Input placeholder="*******"/>
+              </Field>
+              <Button>Entrar</Button>
+              <Link to="/sing-up" className="text-center hover:underline">Não tem conta?</Link>
+            </FieldGroup>
+          </FieldSet>
+        </form>
+      </div>
+    </>
+  );
 }
