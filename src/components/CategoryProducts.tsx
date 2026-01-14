cb161880e3d8dc50ef7c0a597b6dc5a4a0caaae7
unknown <marcoslimado12384@gmail.com>
2026-01-14 15:48:25 -0300

    feat(component): adiconado o componente para sugestão de pesquisa por categoria

---
+++ src/components/CategoryProducts.tsx
@@ -0,0 +1,36 @@
+import { Card, CardDescription, CardTitle } from "./ui/card";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+
+function CategoryProducts() {
+  const category = Array(4).fill(null);
+
+  return (
+    <section className="w-full px-16 mb-16 flex flex-col gap-7">
+      <div>
+        <h1 className="text-3xl leading-none font-semibold">Categorias</h1>
+        <p className="text-muted-foreground text-sm">Explore por categoria</p>
+      </div>
+      <Carousel>
+        <CarouselContent>
+          {category.map((_, index) => (
+            <CarouselItem className="basis-1/4" key={index}>
+              <Card className="relative p-0 h-96 object-center">
+                <img
+                  src="clothes-photo.jpeg"
+                  alt="foto de roupa"
+                  className="rounded-2xl h-full transition-transform duration-100 hover:scale-102"
+                />
+                <div className="absolute bottom-3 left-3">
+                    <CardTitle>Roupas</CardTitle>
+                    <CardDescription>241 produtos</CardDescription>
+                </div>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </section>
+  );
+}
+
+export default CategoryProducts;
